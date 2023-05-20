8ab9a964fd25519ea01c0ed2217818dadbea28b9
mishukmahbub <mishukmahbub@gmail.com>
2023-05-20 21:08:33 +0600

    addtoy initial function done

--- src/pages/AddToy.jsx
+++ src/pages/AddToy.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../contexts/AuthProvider';
+import { useForm } from 'react-hook-form';
 
 const AddToy = () => {
+    const { user } = useContext(AuthContext);
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm();
+
+    const onSubmit = data => {
+        data.skills = selectedOption;
+
+        fetch("http://localhost:5000/addToys", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(data),
+        })
+            .then((res) => res.json())
+            .then((result) => {
+                console.log(result);
+            });
+        console.log(data);
+    };
+
+
     return (
         <div>
-            addtoy
+
         </div>
     );
 };
